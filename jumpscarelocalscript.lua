ca7372c95fdd82f27adc57fa4393597a044b06f9
fan <kuikelprabhav@gmail.com>
2026-05-25 10:34:31 +0545

    Enhance jumpscare script with explanatory comments
    Added comments to clarify functionality and timing for jumpscare script.

---
+++ jumpscarelocalscript.lua
@@ -0,0 +1,25 @@
+local player = game.Players.LocalPlayer
+local part = workspace:WaitForChild("TouchPart") -- the name of the part (in workspace) that jumpscares when touched (can have any name but make sure no other parts are named the same)
+
+local image = script.Parent:WaitForChild("ImageLabel") -- js read the readme
+
+local cooldown = false
+
+part.Touched:Connect(function(hit)
+	if cooldown then return end
+	if hit.Parent ~= player.Character then return end
+
+	cooldown = true
+
+	image.ImageTransparency = 0
+
+	task.wait(0.5) --the time after which jukpscare appears in seconds afder touching the part
+
+	for i = 0, 1, 0.1 do
+		image.ImageTransparency = i
+		task.wait(0.02) --the time the jumpscare (image) stays on the screen
+	end
+
+	task.wait(15) -- basically the cooldown b4 the jumpscare can activate again
+	cooldown = false
+end)
